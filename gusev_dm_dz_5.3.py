ffb673115a126308cbc27351571adbc0d4e843b3
Gusev4-DM <gusdima228@gmail.com>
2021-08-15 22:42:44 +0300

    Сделал дз 5.3

---
+++ gusev_dm_dz_5.3.py
@@ -0,0 +1,12 @@
+from itertools import zip_longest
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+
+gen_turple = ((name, klas) for name, klas in zip_longest(tutors, klasses, fillvalue=None))
+print(list(gen_turple))
