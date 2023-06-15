b5514344471ceb87f5c1d35524edfc85c01e71fb
Dmitriy Gusev <gusdima228@gmail.com>
2023-06-15 20:34:51 +0300

    Added seminar 4 and homework 4

---
+++ Lections/Lections_3.py
@@ -0,0 +1,107 @@
+'''
+
+from modul1 import max_1   1. Альтернативный вариант импорта, импортируя только саму функцию max_1
+
+from modul1 import *       2. * - обозначает импортировать все из этого модуля
+
+import modul1              3. Импортировать модуль полностью, притаком виде импорта надо прописывать нажвание модуля и его функции. Например: print(modul1.max_1(5, 10))
+
+import modul1 as m1        4. Можно заменить название на любое другое и вместо полного названия модуля использовать его. Например: print(m1.max_1(5, 10))
+
+'''
+
+# def sum_numbers(n, y = 'Hello'):
+#     print(y)
+#     summa = 0
+#     for i in range(1, n + 1):
+#         summa += i
+#     return summa
+#     print('стоп')
+
+
+# a  = sum_numbers(5, 'qwerty')
+# print(a)
+
+
+# def sum_str(*args):
+#     res = ''
+#     for i in args:
+#         res += i
+
+#     return(res)
+
+# print(sum_str('q', 'e', 'l'))
+# print(sum_str('q', 'e', 'l', 'r', 'f'))
+# print(sum_str(1, 8, 9)) # ошибка
+
+''' Импорт модуля и обращение к нему, сначала обращаемся к модулю, затем к его функции указывая аргументы '''
+
+# print(max_1(10, 9))
+
+''' Рекурсия это функция вызывающая сама себя '''
+
+def fib(n):
+    if n in [1, 2]:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+list_1 = []
+
+for i in range(1, 10):
+    list_1.append(fib(i))
+
+print(list_1)
+
+'''
+АЛГОРИТМЫ это набор инструкция для выьпоенния некотрых задач. Любой фрагмент программного кода можно назвать алгоритмом.
+Расмотрим 2 самых интеерсных алгоритма сортировок:
+ - Быстрая соритровка
+ - Соритровка слиянием
+'''
+
+# Быстрая сортировка
+
+# def quick_sort(array):
+#     if len(array) <= 1:
+#         return array
+#     else:
+#         pivot = array[0]
+#     less = [i for i in array[1:] if i <= pivot]
+#     greater = [i for i in array[1:] if i > pivot]
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+
+# print(quick_sort([10, 5, 2]))
+
+# Сортировка слиянием
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+list_1 = [1,72,36,7,7,5,4,13,5,96,8,29,4,12]
+
+merge_sort(list_1)
+print(list_1)
+            
+
---
+++ Seminars/Seminar_4/Seminar_4.py
@@ -0,0 +1,106 @@
+import random
+
+'''
+Задача 1. создать кортеж и записать в него 10 случайных чисел.
+'''
+
+# numbers = tuple(random.randint(1, 10) for _ in range(10))
+
+# print(numbers)
+# print(numbers[0])
+
+# numbers = list(numbers) - перевод кортежа в список
+
+# numbers[0] = 15 - изменить кортеж невозможно, число в 0-м индексе не заменится
+
+'''
+Задача 2. Создать кортеж, заполненеый случайными числами. Написать метод, который заменяет элемент в кореже по заданому индексу случайным числом.
+'''
+
+# def change_element(numbers, index):
+#      return numbers[:index] + (random.randint(1, 100), ) + numbers[index + 1:]
+
+# numbers = tuple(random.randint(1, 10) for _ in range(10))
+# index = 4
+
+# print(numbers)
+# numbers = change_element(numbers, index)
+# print(numbers)
+
+'''
+Задача 3. На вход подаются два числа. Напишите метод, который вернет сумму, разность, произведение и частное этих чисел
+'''
+
+# def get_some_numbers(num_1, num_2):
+#     summa = num_1 + num_2
+#     multi = num_1 * num_2
+#     division = num_1 / num_2
+#     return print(f'Сумма - {summa}, Умножение - {multi}, Деление - {division}.')
+
+# num_1 = 5
+# num_2 = 2
+
+# get_some_numbers(num_1, num_2)
+
+###############################################################################
+
+# def calculate(n1, n2):
+#     return n1 + n2, n1 - n2, n1 * n2, n1 / n2
+
+
+# def zadacha_1():
+#     num_f = 9
+#     num_s = 3
+    
+#     summ, deduct, multi, division = calculate(num_f, num_s)
+#     print(summ, deduct, multi, division)
+
+# zadacha_1()
+ 
+###############################################################################
+
+# def method(a, b):
+#     return a + b, a - b, a * b, a / b
+
+# result = method(5, 2)
+# summ = result[0]
+# deduct = result[1]
+# multi = result[2]
+# division = result[3]
+
+# print(summ, deduct, multi, division)
+
+
+'''
+Задача 4. Сгенерируйте список случайных чисел от 1 до 20 состоящий из 10 элементов.
+Удалите из списка дубликаты уже имеющихся элементов. Опеределите, сколько элементов было удалено
+'''
+
+# def get_uniq_numbers():
+#     numbers = [random.randint(1, 10) for _ in range(10)]
+#     deleted_num = len(numbers) - len(set(numbers))
+#     print(f'Количество удаленных элементов - {deleted_num}.')
+
+# get_uniq_numbers()
+
+'''
+Актереов разделили на списки по трем качествам, умные, красивые, сильные. На главную роль нужен атер обладающий всеми тремя каечствами.
+Определите, сколько есть претендентов на главную роль
+'''
+
+beautiful = {"Илья", "Федор", "Семен", "Олег", "Лев", "Антон", "Артем", "Боря", "Стас", "Марк", "Ян"}
+smart = {"Илья", "Георгий", "Лев", "Демьян", "Антон", "Владислав", "Боря", "Стас", "Марк", "Влад"}
+strong = {"Федор", "Георгий", "Олег", "Демьян", "Артем", "Елисей", "Боря", "Стас", "Влад"}
+
+candidates = beautiful.intersection(smart, strong) # .intersection() Позволяет найти  пересечения между итерируемыми объектами set
+print(candidates)
+
+###############################################################################
+
+beautiful = set("Илья Федор Семен Олег Лев Антон Артем Боря Стас Марк Ян".split())
+smart = set("Илья Георгий Лев Демьян Антон Владислав Боря Стас Марк Влад".split())
+strong = set("Федор Георгий Олег Демьян Артем Елисей Боря Стас Влад".split())
+
+top = beautiful & smart & strong # - & работает с множествами, в отличии от AND который работает с логическими операндами
+print(top)
+
