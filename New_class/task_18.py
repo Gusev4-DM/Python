c32afc19ed48a64c824d67439dd5917b3b5ba76a
Dmitriy Gusev <gusdima228@gmail.com>
2024-02-09 02:12:06 +0300

    changes task_18

--- New_class/task_18.py
+++ New_class/task_18.py
@@ -12,9 +12,3 @@ max_l = len(max(my_str, key=len))
 
 for i, el in enumerate(sorted(my_str), 1):
     print(f'{i}. {el:>{max_l}}')
-
-
-text = "vsdsdsdsdsds dfsfdf fdfdfd fdfdfd".split()
-shift = len(max(text))
-for n, el in enumerate(sorted(text), 1):
-    print(f"{n}. {el:>{shift}}")
