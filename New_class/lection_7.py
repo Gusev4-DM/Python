64c85fc19c2b52ae7dda105839fca83c4ac0fffa
Dmitriy Gusev <gusdima228@gmail.com>
2024-03-18 03:12:37 +0300

    update lection_7

--- New_class/lection_7.py
+++ New_class/lection_7.py
@@ -182,3 +182,33 @@ with open('new_data.txt', 'a', encoding='utf-8') as f:
         # res = f.write(f'{line}\n') - Добавляем этот пернос строки таким образом
         print(f'{res = }\n{len(line) = }')
 
+
+# ● Запись методом writelines
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        'Consequatur debitis explicabo laboriosam sint suscipit temporibus veniam?',
+        'Accusantium alias amet fugit iste neque non odit quia saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    f.writelines('\n'.join(text)) # Каждая строка будет записана с новой строки. Сама по себе не добавляет перенос
+
+
+# ● print в файл
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        'Consequatur debitis explicabo laboriosam sint suscipit temporibus veniam?',
+        'Accusantium alias amet fugit iste neque non odit quia saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        print(line, file=f) # можно явно передать файловый объект для печати в файл. Доп.параметр file куда передаю перменную f. Добавляет переход на новую строку
+
+# Другое решение
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        print(line, end='***\n##', file=f) # Добавит в конце строчки у звезды, перенесет строку и нарисует две решетки в тексте
+
+
+
+''' Методы перемещения в файле '''
+
+
+# ● Метод tell
+# Метод tell возвращает текущую позицию в файле
+
