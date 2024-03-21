f9c61c4db38b74ec55e5d7c876ef3d7815f18175
Dmitriy Gusev <gusdima228@gmail.com>
2024-03-21 03:10:25 +0300

    final update lection_7.py

--- New_class/lection_7.py
+++ New_class/lection_7.py
@@ -286,3 +286,120 @@ print(Path.cwd())
 os.mkdir('new_os_dir')
 Path('new_path_dir').mkdir()
 
+# Создание несколько вложенных друг в друга каталогов
+os.makedirs('dir/other_dir/new_os_dir')
+Path('some_dir/dir/new_path_dir').mkdir() # FileNotFoundError
+Path('some_dir/dir/new_path_dir').mkdir(parents=True) # Дополнительный параметр parents=True указывает на необходимость создать всех недостающих родительских каталогов.
+
+
+# ● Удаление каталогов
+os.rmdir('dir') # OSError, папка не пустая
+Path('some_dir').rmdir() # OSError, все так же, папка не пустая
+os.rmdir('dir/other_dir/new_os_dir')
+Path('some_dir/dir/new_path_dir').rmdir()
+
+# Если необходимо удалить каталог со всем его содержимым (вложенные каталоги и файлы), подойдёт функция из модуля shutil
+import shutil
+
+shutil.rmtree('dir/other_dir') # dir останется, other_dir удалиться со всем содержимым
+shutil.rmtree('some_dir') # some_dir будет удален со всем содержимым
+
+
+# ● Формирование пути
+import os
+from pathlib import Path
+
+file_1 = os.path.join(os.getcwd(), 'dir', 'new_file.txt')
+print(f'{file_1 = }\n{file_1}')
+
+file_2 = Path().cwd() / 'dir' / 'new_file.txt'
+print(f'{file_2 = }\n{file_2}')
+
+
+''' Чтение данных о каталогах '''
+
+# Для получения информации о том какие директории и файлы находятся в текущем каталоге можно воспользоваться следующими вариантами кода.
+import os
+from pathlib import Path
+
+print(os.listdir())
+
+p = Path(Path().cwd())
+for obj in p.iterdir():
+    print(obj)
+
+
+# ● Проверка на директорию, файл и ссылку
+import os
+from pathlib import Path
+
+dir_list = os.listdir()
+for obj in dir_list:
+    print(f'{os.path.isdir(obj) = }', end='\t')
+    print(f'{os.path.isfile(obj) = }', end='\t')
+    print(f'{os.path.islink(obj) = }', end='\t')
+    print(f'{obj = }')
+
+p = Path(Path().cwd())
+for obj in p.iterdir():
+    print(f'{obj.is_dir() = }', end='\t')
+    print(f'{obj.is_file() = }', end='\t')
+    print(f'{obj.is_symlink() = }', end='\t')
+    print(f'{obj = }')
+
+
+# ● Обход папок через os.walk()
+import os
+
+for dir_path, dir_name, file_name in os.walk(os.getcwd()):
+    print(f'{dir_path = }\n{dir_name = }\n{file_name = }\n')
+
+
+''' Работа с файлами '''
+# Рассмотрим базовые операции по работе с файлами как с отдельными объектами.
+
+
+# ● Переименование файлов
+import os
+from pathlib import Path
+
+os.rename('old_name.py', 'new_name.py')
+
+p = Path('old_file.py')
+p.rename('new_file.py')
+
+Path('new_file.py').rename('newest_file.py') # такое же как в строке 365 и 366
+
+
+# ● Перемещение файлов
+import os
+from pathlib import Path
+
+os.replace('newest_file.py', os.path.join(os.getcwd(), 'dir', 'new_name.py'))
+old_file = Path('new_name.py')
+new_file = old_file.replace(Path.cwd() / 'new_os_dir' / old_file)
+
+
+# ● Копирование файлов
+# Для копирования файлов лучше всего подходит модуль shutil, который предоставляет ряд высокоуровневых операций.
+
+import shutil
+shutil.copy('one.txt', 'dir') # copy копирует информация из файла, но игнорирует метаданные
+shutil.copy2('two.txt', 'dir') # copy2 попытается скопировать и метаданные
+
+
+# Если стоит задача скопировать каталог со всем его содержимым в новое место, модуль предоставляет функции copytree
+import shutil
+shutil.copytree('dir', 'one_more_dir') # копирует информацию из каталога dir и называет ее one_more_dir
+
+
+# ● Удаление файлов
+import shutil
+shutil.rmtree('dir')
+
+# Если же необходимо удалить один файл, можно воспользоваться следующими вариантами:
+import os
+from pathlib import Path
+os.remove('one_more_dir/one.txt')
+Path('one_more_dir/one_more.txt').unlink()
+
