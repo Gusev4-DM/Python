195c8e26f28a463a6938fb001f6c5bfe066b251e
Dmitriy Gusev <gusdima228@gmail.com>
2024-04-03 02:51:11 +0300

    add lection 8

---
+++ New_class/lection_8.py
@@ -0,0 +1,108 @@
+''' СЕРИАЛИЗАЦИЯ '''
+
+
+''' JSON '''
+
+# Рассмотрим пример JSON файла
+
+{
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": [ # Квардратные скобки а не фигурные 
+        "Shanna@melissa.tv",
+        "antonette@howel.com"
+    ],
+    "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+            "lat": "-43.9509",
+            "lng": "-34.4618"
+        }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+    }
+}
+
+# ● Преобразование JSON в Python
+# json_file = json.load(f) - Загрузка JSON из файла и сохранение в dict или list
+# json_list = json.loads(json_text) - Загрузка JSON из строки и сохранение в dict или list
+
+import json
+
+with open('user.json', 'r', encoding='utf-8') as f:
+    json_file = json.load(f)
+
+print(f'{type(json_file) = }\n{json_file = }')
+print(f'{json_file["name"] = }')
+print(f'{json_file["address"]["geo"] = }')
+print(f'{json_file["email"] = }')
+
+
+# Мы подготовили информацию в виде многострочного str в python и хотим превратить его из JSON строки в объекты Python
+
+json_text = """
+[
+    {
+        "userId": 1,
+        "id": 9,
+        "title": "nesciunt iure omnis dolorem tempora et
+        accusantium",
+        "body": "consectetur animi nesciunt iure dolore"
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "title": "optio molestias id quia eum",
+        "body": "quo et expedita modi cum officia vel magni"
+    },
+    {
+        "userId": 2,
+        "id": 11,
+        "title": "et ea vero quia laudantium autem",
+        "body": "delectus reiciendis molestiae occaecati non minima
+        eveniet qui voluptatibus"
+    },
+    {
+        "userId": 2,
+        "id": 12,
+        "title": "in quibusdam tempore odit est dolorem",
+        "body": "praesentium quia et ea odit et ea voluptas et"
+    }
+]"""
+
+print(f'{type(json_text) = }\n{json_text = }')
+json_list = json.loads(json_text)
+print(f'{type(json_list) = }\t{len(json_list) = }\n{json_list = }')
+
+
+# ● Преобразование Python в JSON
+
+my_dict = {
+    "first_name": "Джон",
+    "last_name": "Смит",
+    "hobbies": ["кузнечное дело", "программирование", "путешествия"],
+    "age": 35,
+    "children": [
+        {
+            "first_name": "Алиса",
+            "age": 5
+        },
+        {
+            "first_name": "Маруся",
+            "age": 3
+        }
+    ]
+}
+
+print(f'{type(my_dict) = }\n{my_dict = }')
+with open('new_user.json', 'w') as f:
+    json.dump(my_dict, f)
